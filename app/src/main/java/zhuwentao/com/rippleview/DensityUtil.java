115f778c43016864d603cb85cedc9b3d267814ae
justinelock <justlookxsite@mail.com>
2020-08-27 01:06:23 +0800

    update

--- app/src/main/java/zhuwentao/com/rippleview/DensityUtil.java
+++ app/src/main/java/zhuwentao/com/rippleview/DensityUtil.java
@@ -10,9 +10,10 @@ public class DensityUtil {
 
     /**
      * 根据手机的分辨率从 dp 的单位 转成为 px(像素)
-     * @param context   上下文
-     * @param dpValue   dp值
-     * @return  px值
+     *
+     * @param context 上下文
+     * @param dpValue dp值
+     * @return px值
      */
     public static int dip2px(Context context, float dpValue) {
         final float scale = context.getResources().getDisplayMetrics().density;
@@ -21,9 +22,10 @@ public class DensityUtil {
 
     /**
      * 根据手机的分辨率从 px(像素) 的单位 转成为 dp
-     * @param context   上下文
-     * @param pxValue   px值
-     * @return  dp值
+     *
+     * @param context 上下文
+     * @param pxValue px值
+     * @return dp值
      */
     public static int px2dip(Context context, float pxValue) {
         final float scale = context.getResources().getDisplayMetrics().density;
